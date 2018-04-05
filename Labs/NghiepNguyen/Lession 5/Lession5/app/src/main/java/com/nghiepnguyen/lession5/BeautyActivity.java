7a7e92554c370d9120ad23614647a40dae9b54b4
Nghiep Nguyen <nghiepnguyenquoc@gmail.com>
2018-04-05 14:26:32 -0500

    add lab5

---
+++ Labs/NghiepNguyen/Lession 5/Lession5/app/src/main/java/com/nghiepnguyen/lession5/BeautyActivity.java
@@ -0,0 +1,30 @@
+package com.nghiepnguyen.lession5;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class BeautyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beauty);
+
+        Spinner beauty_spinner = findViewById(R.id.beauty_spinner);
+        beauty_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
