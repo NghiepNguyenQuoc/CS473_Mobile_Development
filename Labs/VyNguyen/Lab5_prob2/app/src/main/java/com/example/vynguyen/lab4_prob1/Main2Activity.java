7a6785fd320b52f73f004fb5d10016f979a0acea
lecatvynguyen <37512232+lecatvynguyen@users.noreply.github.com>
2018-04-10 01:21:32 -0500

    Add lab4_prob2 design, modify lab5_prob2 using actionbar instead of toolbar

--- Labs/VyNguyen/Lab5_prob2/app/src/main/java/com/example/vynguyen/lab4_prob1/Main2Activity.java
+++ Labs/VyNguyen/Lab5_prob2/app/src/main/java/com/example/vynguyen/lab4_prob1/Main2Activity.java
@@ -1,19 +1,25 @@
 package com.example.vynguyen.lab4_prob1;
 
-import android.app.ActionBar;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.drm.DrmStore;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
-public class Main2Activity extends Activity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +28,62 @@ public class Main2Activity extends Activity {
 
         // Received intent and data
         Intent intent = getIntent();
+        final Activity context = this;
         String email = intent.getStringExtra("email");
 
         // Set welcome string
         TextView tv = (TextView)findViewById(R.id.welcomUser);
         tv.setText("Welcome " + email);
+
+        // Action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.BLUE));
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.TabListener listener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                String tabName = tab.getText().toString();
+
+                Intent intent;
+                switch (tabName) {
+                    case "Electronics":
+                        intent = new Intent(context, ElectronicViewActivity.class);
+                        startActivity(intent);
+                        break;
+                    case "Clothing":
+                        intent = new Intent(context, ClothingActivity.class);
+                        startActivity(intent);
+                        break;
+                    case "Beauty":
+                        intent = new Intent(context, BeautyActivity.class);
+                        startActivity(intent);
+                        break;
+                    case "Food":
+                        intent = new Intent(context, FoodActivity.class);
+                        startActivity(intent);
+                        break;
+                    case "Logout":
+                        exitClick();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+        };
+        actionBar.addTab(actionBar.newTab().setText("Electronics").setTabListener(listener), false);
+        actionBar.addTab(actionBar.newTab().setText("Clothing").setTabListener(listener), false);
+        actionBar.addTab(actionBar.newTab().setText("Beauty").setTabListener(listener), false);
+        actionBar.addTab(actionBar.newTab().setText("Food").setTabListener(listener), false);
+        actionBar.addTab(actionBar.newTab().setText("Logout").setTabListener(listener),false);
 
     }
 
@@ -34,19 +91,19 @@ public class Main2Activity extends Activity {
         String catName = "";
         switch (view.getId()) {
             case R.id.electronics:
-            case R.id.electricToolBar:
+            //case R.id.electricToolBar:
                 catName = "Electronics";
                 break;
             case R.id.clothing:
-            case R.id.clothingToolBar:
+            //case R.id.clothingToolBar:
                 catName = "Clothing";
                 break;
             case R.id.beauty:
-            case R.id.beautyToolBar:
+            //case R.id.beautyToolBar:
                 catName = "Beauty";
                 break;
             case R.id.food:
-            case R.id.foodToolBar:
+            //case R.id.foodToolBar:
                 catName = "Food";
                 break;
         }
@@ -62,25 +119,25 @@ public class Main2Activity extends Activity {
         // Go to product activity
         switch (view.getId()) {
             case R.id.electronics:
-            case R.id.electricToolBar:
+            //case R.id.electricToolBar:
                 // Go to electronic activity
                 intent = new Intent(this, ElectronicViewActivity.class);
                 startActivity(intent);
                 break;
             case R.id.clothing:
-            case R.id.clothingToolBar:
+            //case R.id.clothingToolBar:
                 // Go to clothing activity
                 intent = new Intent(this, ClothingActivity.class);
                 startActivity(intent);
                 break;
             case R.id.beauty:
-            case R.id.beautyToolBar:
+            //case R.id.beautyToolBar:
                 // Go to beauty activity
                 intent = new Intent(this, BeautyActivity.class);
                 startActivity(intent);
                 break;
             case R.id.food:
-            case R.id.foodToolBar:
+            //case R.id.foodToolBar:
                 // Go to food activity
                 intent = new Intent(this, FoodActivity.class);
                 startActivity(intent);
@@ -88,7 +145,7 @@ public class Main2Activity extends Activity {
         }
     }
 
-    public  void  exitClick(View view) {
+    public  void  exitClick() {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
         alert.setTitle("Exit Dialog");
