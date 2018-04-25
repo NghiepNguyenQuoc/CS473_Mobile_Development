9462d1552e8909ddd7a951fe5a0c34c064136810
Nghiep Nguyen <nghiepnguyenquoc@gmail.com>
2018-04-25 15:24:07 -0500

    convert Phrase class to Kotlin

---
+++ Project/PhraseBook/app/src/main/java/nghiepnguyen/com/phrasebook/model/Phrase.kt
@@ -0,0 +1,17 @@
+package nghiepnguyen.com.phrasebook.model
+
+class Phrase {
+    var id: Int = 0
+    var idCategory: Int = 0
+    var phrase: String? = null
+    var description1: String? = null
+    var description2: String? = null
+    var description3: String? = null
+    var pronunciation: String? = null
+    var description4: String? = null
+    var mean: String? = null
+    var description5: String? = null
+    var sound: String? = null
+    var number: Int = 0
+
+}
