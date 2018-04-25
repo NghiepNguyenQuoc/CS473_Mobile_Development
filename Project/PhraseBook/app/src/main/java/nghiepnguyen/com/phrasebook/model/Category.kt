5c0b7171814d88f6597d5049f619f2bbfed725f1
Nghiep Nguyen <nghiepnguyenquoc@gmail.com>
2018-04-25 15:25:44 -0500

    convert Category class to Kotlin

---
+++ Project/PhraseBook/app/src/main/java/nghiepnguyen/com/phrasebook/model/Category.kt
@@ -0,0 +1,10 @@
+package nghiepnguyen.com.phrasebook.model
+
+class Category {
+    var id: Int = 0
+    var namecategory: String? = null
+    var img: String? = null
+    var lock: Int = 0
+    var description: String? = null
+
+}
