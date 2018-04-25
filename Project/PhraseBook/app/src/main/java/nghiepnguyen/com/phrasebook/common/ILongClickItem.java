ae3491cd102ebdadd7e7e7f1ece00aed9f715d6a
Nghiep Nguyen <nghiepnguyenquoc@gmail.com>
2018-04-25 15:16:56 -0500

    convert ILongClickItem to Kotlin

--- Project/PhraseBook/app/src/main/java/nghiepnguyen/com/phrasebook/common/ILongClickItem.java
+++
@@ -1,7 +0,0 @@
-package nghiepnguyen.com.phrasebook.common;
-
-import nghiepnguyen.com.phrasebook.model.Phrase;
-
-public interface ILongClickItem {
-    void onLongClickItem(Phrase item);
-}
---
+++ Project/PhraseBook/app/src/main/java/nghiepnguyen/com/phrasebook/common/ILongClickItem.kt
@@ -0,0 +1,7 @@
+package nghiepnguyen.com.phrasebook.common
+
+import nghiepnguyen.com.phrasebook.model.Phrase
+
+interface ILongClickItem {
+    fun onLongClickItem(item: Phrase)
+}
